6b9a050c047e9f7078fda6bca8ca690664328d99
Giuliano Quites <giulianoquites@gmail.com>
2026-03-25 15:13:13 -0300

    Update clone.go
    Signed-off-by: Giuliano Quites <giulianoquites@gmail.com>

--- pkg/virtctl/clone/clone.go
+++ pkg/virtctl/clone/clone.go
@@ -5,90 +5,73 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	clonev1 "kubevirt.io/api/clone/v1beta1"
-
-	"kubevirt.io/kubevirt/pkg/virtctl/templates"
+	"kubevirt.io/client-go/kubecli"
 )
 
-func NewCloneCommand(clientGetter templates.KubecliGetter) *cobra.Command {
+func NewCloneCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "clone",
 		Short: "Clona uma VirtualMachine.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return run(clientGetter, cmd)
+			return run(cmd)
 		},
 	}
 
-	// Flags do comando
 	cmd.Flags().String("name", "", "Nome do recurso de clone")
 	cmd.Flags().String("source", "", "Nome da VM de origem")
-	cmd.Flags().String("target", "", "Nome da nova VM (alvo)")
-	cmd.Flags().String("source-namespace", "", "Namespace da VM de origem (opcional)")
-	cmd.Flags().String("target-namespace", "", "Namespace da VM alvo (opcional)")
+	cmd.Flags().String("target", "", "Nome da VM destino")
 
-	cmd.MarkFlagRequired("source")
-	cmd.MarkFlagRequired("target")
+	_ = cmd.MarkFlagRequired("source")
+	_ = cmd.MarkFlagRequired("target")
 
 	return cmd
 }
 
-func run(clientGetter templates.KubecliGetter, cmd *cobra.Command) error {
-	virtClient, err := clientGetter.Get()
+func run(cmd *cobra.Command) error {
+	virtClient, err := kubecli.GetKubevirtClient()
 	if err != nil {
 		return err
 	}
 
-	currentNamespace, _, _ := clientGetter.ToRawKubeConfigLoader().Namespace()
+	namespace, _, err := kubecli.GetNamespace()
+	if err != nil {
+		return err
+	}
 
 	source, _ := cmd.Flags().GetString("source")
 	target, _ := cmd.Flags().GetString("target")
 	name, _ := cmd.Flags().GetString("name")
-	sourceNamespace, _ := cmd.Flags().GetString("source-namespace")
-	targetNamespace, _ := cmd.Flags().GetString("target-namespace")
 
 	if name == "" {
 		name = fmt.Sprintf("clone-%s", source)
 	}
 
-	if sourceNamespace == "" {
-		sourceNamespace = currentNamespace
-	}
-
-	if targetNamespace == "" {
-		targetNamespace = currentNamespace
-	}
-
-	group := "kubevirt.io"
-
-	// Definindo o objeto VirtualMachineClone
 	vmClone := &clonev1.VirtualMachineClone{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
-			Namespace: targetNamespace,
+			Namespace: namespace,
 		},
 		Spec: clonev1.VirtualMachineCloneSpec{
-			Source: &clonev1.TypedObjectReference{
-				APIGroup:  &group,
-				Kind:      "VirtualMachine",
-				Name:      source,
-				Namespace: sourceNamespace,
+			Source: &corev1.TypedLocalObjectReference{
+				Kind: "VirtualMachine",
+				Name: source,
 			},
-			Target: &clonev1.TypedObjectReference{
-				APIGroup:  &group,
-				Kind:      "VirtualMachine",
-				Name:      target,
-				Namespace: targetNamespace,
+			Target: &corev1.TypedLocalObjectReference{
+				Kind: "VirtualMachine",
+				Name: target,
 			},
 		},
 	}
 
-	// Enviando para a API do Kubernetes
 	result := &clonev1.VirtualMachineClone{}
+
 	err = virtClient.RestClient().Post().
 		Resource("virtualmachineclones").
-		Namespace(targetNamespace).
+		Namespace(namespace).
 		Body(vmClone).
 		Do(context.Background()).
 		Into(result)
@@ -97,6 +80,6 @@ func run(clientGetter templates.KubecliGetter, cmd *cobra.Command) error {
 		return fmt.Errorf("falha ao criar clone: %v", err)
 	}
 
-	fmt.Printf("Recurso VirtualMachineClone '%s' criado com sucesso!\n", result.Name)
+	fmt.Printf("VirtualMachineClone '%s' criado com sucesso!\n", result.Name)
 	return nil
 }
