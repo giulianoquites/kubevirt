d021d3face77c05be0d451232a99eab66e8acd60
Giuliano Quites <giulianoquites@gmail.com>
2026-03-26 20:37:32 -0300

    Update clone.go
    Signed-off-by: Giuliano Quites <giulianoquites@gmail.com>

--- pkg/virtctl/clone/clone.go
+++ pkg/virtctl/clone/clone.go
@@ -5,130 +5,129 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	clonev1 "kubevirt.io/api/clone/v1beta1"
 	"kubevirt.io/kubevirt/pkg/virtctl/clientconfig"
 )
 
+type cloneOptions struct {
+	namespace string
+	source    string
+	target    string
+
+	labelFilters      []string
+	annotationFilters []string
+
+	templateLabelFilters      []string
+	templateAnnotationFilters []string
+
+	newMacAddresses map[string]string
+	newSMBiosSerial string
+
+	patches []string
+}
+
 func NewCommand() *cobra.Command {
-
-	var (
-		name   string
-		source string
-		target string
-
-		macMap                    map[string]string
-		serial                    string
-		labelFilters              []string
-		annotationFilters         []string
-		templateLabelFilters      []string
-		templateAnnotationFilters []string
-		patches                   []string
-	)
+	opts := &cloneOptions{
+		newMacAddresses: map[string]string{},
+	}
 
 	cmd := &cobra.Command{
 		Use:   "clone",
-		Short: "Clone a VirtualMachine",
-		Long:  "Clone a VirtualMachine using a VirtualMachineClone resource",
-
-		Example: `
-virtctl clone --source vm1 --target vm2
-virtctl clone --source vm1 --target vm2 --mac eth0=02:00:00:aa:bb:cc
-`,
-
+		Short: "Clone a VirtualMachine using a VirtualMachineClone resource",
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			virtClient, namespace, _, err := clientconfig.ClientAndNamespaceFromContext(cmd.Context())
-			if err != nil {
-				return err
-			}
-
-			if name == "" {
-				name = fmt.Sprintf("clone-%s", source)
-			}
-
-			vmClone := &clonev1.VirtualMachineClone{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      name,
-					Namespace: namespace,
-				},
-				Spec: clonev1.VirtualMachineCloneSpec{
-					Source: &v1.TypedLocalObjectReference{
-						APIGroup: strPtr("kubevirt.io"),
-						Kind:     "VirtualMachine",
-						Name:     source,
-					},
-					Target: &v1.TypedLocalObjectReference{
-						APIGroup: strPtr("kubevirt.io"),
-						Kind:     "VirtualMachine",
-						Name:     target,
-					},
-				},
-			}
-
-			// opcionais
-			if len(labelFilters) > 0 {
-				vmClone.Spec.LabelFilters = labelFilters
-			}
-			if len(annotationFilters) > 0 {
-				vmClone.Spec.AnnotationFilters = annotationFilters
-			}
-			if len(templateLabelFilters) > 0 || len(templateAnnotationFilters) > 0 {
-				vmClone.Spec.Template = clonev1.VirtualMachineCloneTemplateFilters{
-					LabelFilters:      templateLabelFilters,
-					AnnotationFilters: templateAnnotationFilters,
-				}
-			}
-			if len(macMap) > 0 {
-				vmClone.Spec.NewMacAddresses = macMap
-			}
-			if serial != "" {
-				vmClone.Spec.NewSMBiosSerial = &serial
-			}
-			if len(patches) > 0 {
-				vmClone.Spec.Patches = patches
-			}
-
-			err = virtClient.RestClient().Post().
-				Resource("virtualmachineclones").
-				Namespace(namespace).
-				Body(vmClone).
-				Do(context.Background()).
-				Error()
-
-			if err != nil {
-				return fmt.Errorf("failed to create clone: %v", err)
-			}
-
-			fmt.Printf("Clone '%s' created\n", name)
-			return nil
+			return run(cmd.Context(), opts)
 		},
 	}
 
-	// 🔥 FLAGS DEFINIDAS AQUI (ESSENCIAL)
+	cmd.Flags().StringVar(&opts.source, "source", "", "Source VM name")
+	cmd.Flags().StringVar(&opts.target, "target", "", "Target VM name")
 
-	cmd.Flags().StringVar(&name, "name", "", "Clone resource name")
-	cmd.Flags().StringVar(&source, "source", "", "Source VM name")
-	cmd.Flags().StringVar(&target, "target", "", "Target VM name")
+	cmd.Flags().StringSliceVar(&opts.labelFilters, "labels", nil, "Label filters")
+	cmd.Flags().StringSliceVar(&opts.annotationFilters, "annotations", nil, "Annotation filters")
 
-	cmd.Flags().StringToStringVar(&macMap, "mac", nil, "New MAC addresses (eth0=xx:xx:xx)")
-	cmd.Flags().StringVar(&serial, "serial", "", "New SMBIOS serial")
+	cmd.Flags().StringSliceVar(&opts.templateLabelFilters, "template-labels", nil, "Template label filters")
+	cmd.Flags().StringSliceVar(&opts.templateAnnotationFilters, "template-annotations", nil, "Template annotation filters")
 
-	cmd.Flags().StringSliceVar(&labelFilters, "label-filter", nil, "Label filters")
-	cmd.Flags().StringSliceVar(&annotationFilters, "annotation-filter", nil, "Annotation filters")
+	cmd.Flags().StringToStringVar(&opts.newMacAddresses, "mac", nil, "MAC addresses (eth0=xx:xx:xx)")
+	cmd.Flags().StringVar(&opts.newSMBiosSerial, "serial", "", "SMBIOS serial")
 
-	cmd.Flags().StringSliceVar(&templateLabelFilters, "template-label-filter", nil, "Template label filters")
-	cmd.Flags().StringSliceVar(&templateAnnotationFilters, "template-annotation-filter", nil, "Template annotation filters")
+	cmd.Flags().StringSliceVar(&opts.patches, "patch", nil, "JSON patches")
 
-	cmd.Flags().StringSliceVar(&patches, "patch", nil, "JSON patches")
+	cmd.Flags().StringVarP(&opts.namespace, "namespace", "n", "", "Namespace")
 
-	// REQUIRED
-	cmd.MarkFlagRequired("source")
-	cmd.MarkFlagRequired("target")
+	_ = cmd.MarkFlagRequired("source")
+	_ = cmd.MarkFlagRequired("target")
 
 	return cmd
+}
+
+func run(ctx context.Context, opts *cloneOptions) error {
+	virtClient, namespace, _, err := clientconfig.ClientAndNamespaceFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	if opts.namespace != "" {
+		namespace = opts.namespace
+	}
+
+	vmClone := &clonev1.VirtualMachineClone{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      opts.target + "-clone",
+			Namespace: namespace,
+		},
+		Spec: clonev1.VirtualMachineCloneSpec{
+			Source: &corev1.TypedLocalObjectReference{
+				APIGroup: strPtr("kubevirt.io"),
+				Kind:     "VirtualMachine",
+				Name:     opts.source,
+			},
+			Target: &corev1.TypedLocalObjectReference{
+				APIGroup: strPtr("kubevirt.io"),
+				Kind:     "VirtualMachine",
+				Name:     opts.target,
+			},
+		},
+	}
+
+	// opcionais
+	if len(opts.labelFilters) > 0 {
+		vmClone.Spec.LabelFilters = opts.labelFilters
+	}
+
+	if len(opts.annotationFilters) > 0 {
+		vmClone.Spec.AnnotationFilters = opts.annotationFilters
+	}
+
+	if len(opts.templateLabelFilters) > 0 || len(opts.templateAnnotationFilters) > 0 {
+		vmClone.Spec.Template = clonev1.VirtualMachineCloneTemplateFilters{
+			LabelFilters:      opts.templateLabelFilters,
+			AnnotationFilters: opts.templateAnnotationFilters,
+		}
+	}
+
+	if len(opts.newMacAddresses) > 0 {
+		vmClone.Spec.NewMacAddresses = opts.newMacAddresses
+	}
+
+	if opts.newSMBiosSerial != "" {
+		vmClone.Spec.NewSMBiosSerial = strPtr(opts.newSMBiosSerial)
+	}
+
+	if len(opts.patches) > 0 {
+		vmClone.Spec.Patches = opts.patches
+	}
+
+	_, err = virtClient.VirtualMachineClone(namespace).Create(ctx, vmClone, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to create clone: %v", err)
+	}
+
+	fmt.Printf("Clone %s created successfully\n", vmClone.Name)
+	return nil
 }
 
 func strPtr(s string) *string {
